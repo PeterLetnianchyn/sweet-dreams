fe8c29d4787a7e87bdbed1ab3b2b05b22909e19e
Peter <katalol95@gmail.com>
2018-12-03 16:49:26 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.querySelector('.about-us').addEventListener('click', getHtmlAjax);
+document.querySelector('.about-us').addEventListener('click', getHtmlAjaxAboutUs);
 
-function getHtmlAjax() {
+function getHtmlAjaxAboutUs() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -8,5 +8,43 @@ function getHtmlAjax() {
         }
     }
     xhr.open('GET', 'about-us.html', true);
+    xhr.send();
+}
+
+document.querySelector('.production').addEventListener('click', getHtmlAjaxProduction);
+
+function getHtmlAjaxProduction() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+           document.querySelector('#main').innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open('GET', 'production.html', true);
+    xhr.send();
+}
+document.querySelector('.our-team').addEventListener('click', getHtmlAjaxOurTeam);
+
+function getHtmlAjaxOurTeam() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+           document.querySelector('#main').innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open('GET', 'our-team.html', true);
+    xhr.send();
+}
+
+document.querySelector('.contact').addEventListener('click', getHtmlAjaxContact);
+
+function getHtmlAjaxContact() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+           document.querySelector('#main').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'contact.html', true);
     xhr.send();
 }
